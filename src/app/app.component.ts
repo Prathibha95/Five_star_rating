4c1c18fce922e28de347e177ecb0ceb69371427a
prathibha <prathibha.17@itfaq.mrt.ac.lk>
2021-05-24 17:04:38 +0530

    first_commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rating-app';
+public rating1: any;
+public rating2: any;
+
+
+  ngOnInit(): void {
+    this.rating1 = 0;
+    this.rating2 = 0;
+  }
+
+  submitForm(form: NgForm) {
+    form.resetForm();
+    console.log("Rating 1: " + this.rating1);
+    console.log("Rating 1: " + this.rating2);
+    this.rating1 = 0;
+    this.rating2 = 0;
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MDBBootstrapModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
